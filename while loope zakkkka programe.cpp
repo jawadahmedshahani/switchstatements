8e17877649032530d97c41bbc2f873f354bb1d72
jawadahmedshahani <70814895+jawadahmedshahani@users.noreply.github.com>
2020-09-05 09:58:49 -0700

    add new file

---
+++ while loope zakkkka programe.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+int b=6;
+int a;
+
+while(b>5)
+{
+
+    cout<<"jawad"<<endl;
+    cout<<"enter any key to run"<<endl;
+    cout<<"enter any key to stop the program"<<endl;
+    cin>>a;
+    if(a==5)
+     break;
+
+	
+  }
+return 0;
+
+
+}
+ 
+ 
+
+
+
+
+
+	
+	
+	
+	
+
